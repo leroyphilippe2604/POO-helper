6539ba3a145c3dee3e375ad965ebfd5d470914f5
leroyphilippe2604 <leroyphilippe2604@gmail.com>
2020-12-09 10:44:48 +0100

    voiture

---
+++ htdocs/parc.php
@@ -0,0 +1,14 @@
+<?php
+    require_once "voiture.php";
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>parc voiture</title>
+</head>
+<body>
+    
+</body>
+</html>
