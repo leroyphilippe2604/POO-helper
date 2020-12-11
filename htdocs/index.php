4c22d33ef85caecaf3f68248db8a7ce2378bb89f
leroyphilippe2604 <leroyphilippe2604@gmail.com>
2020-12-11 09:27:40 +0100

    fin

--- htdocs/index.php
+++ htdocs/index.php
@@ -21,8 +21,8 @@ echo $form->create("post");
 echo $form->input('text', 'user');
 echo $form->select("genre", ["male", "female","Unicorn"]);
 echo $form->textarea(10, "Dis nous bonjour");
-echo $form->checkInput("radio", "genre", ["male", "female", "unicorn"]);
-echo $form->checkInput("checkbox", "genre", ["male", "female", "unicorn"]);
+echo $form->checkInput("radio", "genre", ["male", "female", "Canard"]);
+echo $form->checkInput("checkbox", "genre", ["male", "female", "Unicorn"]);
 echo $form->submit();
 ?>
 
